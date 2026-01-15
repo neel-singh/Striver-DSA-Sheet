d486958f4bf54ab1bc233a2948f3122dc310ce48
neel-singh <neelsingh0919@gmail.com>
2026-01-15 23:46:47 +0530

    Initialize Two Sum class with comments
    Added initial comments and structure for Two Sum solution.

---
+++ Arrays/Medium/TwoSum.java
@@ -0,0 +1,26 @@
+// probelem: Two Sum
+// pattern: traversing
+// Tiime Complexity: O(n^2)
+// Space complexity: O(1)
+
+import java.util.*;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] res = new int[2];
+        
+        for(int i = 0; i < nums.length-1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                int sum = nums[i] + nums[j];
+
+                if(sum == target){
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+
+        return res;
+    }
+}
