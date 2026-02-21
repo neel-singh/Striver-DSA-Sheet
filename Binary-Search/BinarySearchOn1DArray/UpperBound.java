d432a09653a9005f0ec702b3abbbd6bf1d430eab
neel-singh <neelsingh0919@gmail.com>
2026-02-21 14:36:51 +0530

    Add UpperBound class with binary search implementation
    Implement the upper bound function using binary search.

---
+++ Binary-Search/BinarySearchOn1DArray/UpperBound.java
@@ -0,0 +1,27 @@
+// Problem: Implement Upper Bound
+// Pattern: Binary Search
+// Time Complexity: O(logn)
+// Space Complexity: O(1)
+
+class UpperBound {
+    int upperBound(int[] nums, int target) {
+        // code here
+        int low = 0;
+        int high = nums.length-1;
+        int res = nums.length;
+        
+        while(low <= high){
+            int guess = (low + high) / 2;
+            
+            if(nums[guess] <= target){
+                low = guess + 1;
+            }
+            else{
+                res = guess;
+                high = guess - 1;
+            }
+        }
+        
+        return res;
+    }
+}
