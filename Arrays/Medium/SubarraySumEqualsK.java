b955faaa6049303f16da6ec976de1665c64fa1f6
neel-singh <neelsingh0919@gmail.com>
2026-04-23 17:08:33 +0530

    Create SubarraySumEqualsK.java

---
+++ Arrays/Medium/SubarraySumEqualsK.java
@@ -0,0 +1,36 @@
+// probelem: Subarray Sum Equals K
+// pattern: prefix + HashMap
+// Tiime Complexity: O(n)
+// Space complexity: O(n)
+
+import java.util.*;
+
+class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int count = 0;
+
+        map.put(0, 1);
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+
+            int diff = sum-k;
+
+            if(map.containsKey(diff)){
+                count += map.get(diff);
+            }
+
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum)+1);
+            }
+            else{
+                map.put(sum, 1);
+            }
+        }
+
+        return count;
+    }
+}
